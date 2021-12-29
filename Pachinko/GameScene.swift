dab78aa3efebc6855d9210aa54685130da898db3
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2021-12-29 21:55:52 +0300

    Added collisions detection

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -8,81 +8,106 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+
+  //MARK: - Scene presentation methods
+  override func didMove(to view: SKView) {
+    let background = SKSpriteNode(imageNamed: "background")
+    background.position = CGPoint(x: 512, y: 384)
+    background.blendMode = .replace
+    background.zPosition = -1
+    addChild(background)
+    physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    physicsWorld.contactDelegate = self
+
+    makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+    makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+    makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+    makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
+
+    makeBouncer(at: CGPoint(x: 0, y: 0))
+    makeBouncer(at: CGPoint(x: 256, y: 0))
+    makeBouncer(at: CGPoint(x: 512, y: 0))
+    makeBouncer(at: CGPoint(x: 768, y: 0))
+    makeBouncer(at: CGPoint(x: 1024, y: 0))
+
+  }
+
+  //MARK: - UIMethods
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let location = touch.location(in: self)
+    let ball = SKSpriteNode(imageNamed: "ballRed")
+    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+    ball.physicsBody?.restitution = 0.4
+    ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+
+    ball.position = location
+    ball.name = "ball"
+    addChild(ball)
+  }
+
+  func makeBouncer(at position: CGPoint) {
+    let  bouncer = SKSpriteNode(imageNamed: "bouncer")
+    bouncer.position = position
+    bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+    bouncer.physicsBody?.isDynamic = false
+    addChild(bouncer)
+  }
+
+  func makeSlot(at position: CGPoint, isGood: Bool) {
+    var slotBase: SKSpriteNode
+    var slotGlow: SKSpriteNode
+    if isGood {
+      slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+      slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+      slotBase.name = "good"
+    } else {
+      slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+      slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+      slotBase.name = "bad"
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    slotBase.position = position
+    slotGlow.position = position
+
+    slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+    slotBase.physicsBody?.isDynamic = false
+
+
+
+    addChild(slotBase)
+    addChild(slotGlow)
+
+    let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+    let spinForever = SKAction.repeatForever(spin)
+    slotGlow.run(spinForever)
+  }
+
+  //MARK: - Collisions methods
+  func collision(between ball: SKNode, object: SKNode) {
+    if object.name == "good" {
+      destroy(ball: ball)
+    } else if object.name == "bad"{
+      destroy(ball: ball)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+  }
+
+  func destroy(ball: SKNode) {
+    ball.removeFromParent()
+  }
+
+  func didBegin(_ contact: SKPhysicsContact) {
+
+    guard let nodeA = contact.bodyA.node else { return }
+    guard let nodeB = contact.bodyB.node else { return }
+
+    if nodeA.name == "ball" {
+      collision(between: nodeA, object: nodeB)
+    } else if nodeB.name == "ball" {
+      collision(between: nodeB, object: nodeA)
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
+
+
 }
